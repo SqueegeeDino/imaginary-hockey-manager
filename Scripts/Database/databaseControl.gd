0df433e55b3460f0fbc062f4905c1a1a776b04de
SqueegeeDinoToy <viper970@gmail.com>
2026-03-11 08:58:24 -0600

    Select player scores with JOIN and filter
    Update custom select to query the players' scores via a JOIN with playerInfo and filter results by the scoreLine input (where score > scoreLine.text). Also remove several extraneous blank lines in the file for minor cleanup.

--- Scripts/Database/databaseControl.gd
+++ Scripts/Database/databaseControl.gd
@@ -19,7 +19,6 @@ func _on_btn_create_table_pressed() -> void:
 	database.create_table("players", table)
 	print("Table created")
 
-
 func _on_btn_insert_data_pressed() -> void:
 	print("Score: ", int(scoreLine.text))
 	var data = {
@@ -29,22 +28,18 @@ func _on_btn_insert_data_pressed() -> void:
 	
 	database.insert_row("players", data)
 
-
 func _on_btn_select_data_pressed() -> void:
 	print(database.select_rows("players", "id > 0", ["*"]))
-
 
 func _on_btn_update_data_pressed() -> void:
 	database.update_rows("players", "name = '" + nameLine.text + "'", {"score": int(scoreLine.text)})
 	pass # Replace with function body.
 
-
 func _on_btn_delete_data_pressed() -> void:
 	database.delete_rows("players", "id > 0")
 
-
 func _on_btn_custom_select_pressed() -> void:
-	database.query("select * from players
+	database.query("select score from players
 JOIN playerInfo on playerInfo.id = players.playerInfoID
 where score > " + scoreLine.text)
 	for i in database.query_result:
